20af14aa41a78d47dc43959aa33e60d381ddd027
volk7 <carypalmer20@yahoo.com>
2024-09-25 16:23:23 -0700

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
