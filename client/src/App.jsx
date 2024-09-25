b3c6050f94bb897ca1e4ebb513caa694047b9e96
volk7 <carypalmer20@yahoo.com>
2024-09-25 15:19:53 -0700

    Created Header components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import Signin from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import About from './pages/About';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -16,5 +18,5 @@ export default function App() {
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
